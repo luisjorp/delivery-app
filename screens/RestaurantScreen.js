5fb3c1f42e7549c19d9b30c6bad19a939e767262
LuisJo Ruano <luis.jose@arcadalabs.com>
2022-09-14 16:46:47 -0600

    ADD: BASKET pop up

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -4,6 +4,9 @@ import {useNavigation, useRoute} from "@react-navigation/native";
 import {urlFor} from "../sanity";
 import {Feather} from "@expo/vector-icons";
 import DishRow from "../components/DishRow";
+import BasketIcon from "../components/BasketIcon";
+import {useSelector} from "react-redux";
+import {selectBasketItems} from "../features/basketSlice";
 
 const RestaurantScreen = () => {
   const navigation = useNavigation()
@@ -13,6 +16,9 @@ const RestaurantScreen = () => {
     }
   } = useRoute()
 
+  const items = useSelector(selectBasketItems)
+
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -20,57 +26,60 @@ const RestaurantScreen = () => {
   }, [])
 
   return (
-    <ScrollView>
-      <View className="relative">
-        <Image
-          source={{uri: urlFor(imgUrl).url()}}
-          className="w-full h-56 bg-gray-300 p-4"/>
-        <TouchableOpacity
-          onPress={() => navigation.goBack()}
-          className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full">
-          <Feather name="arrow-left" size={20} color="#00CCBB"/>
-        </TouchableOpacity>
-      </View>
-      <View className="bg-white">
-        <View className="px-4 pt-4">
-          <Text className="text-3xl font-bold">{title}</Text>
-          <View className="flex-row space-x-2 my-1">
-            <View className="flex-row items-center space-x-1">
-              <Feather size={22} name="star" color="#00CCBB"/>
-              <Text className={"text-gray-500"}>{rating} · {genre}
-              </Text>
+    <>
+      {items.length > 0 && <BasketIcon/>}
+      <ScrollView>
+        <View className="relative">
+          <Image
+            source={{uri: urlFor(imgUrl).url()}}
+            className="w-full h-56 bg-gray-300 p-4"/>
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full">
+            <Feather name="arrow-left" size={20} color="#00CCBB"/>
+          </TouchableOpacity>
+        </View>
+        <View className="bg-white">
+          <View className="px-4 pt-4">
+            <Text className="text-3xl font-bold">{title}</Text>
+            <View className="flex-row space-x-2 my-1">
+              <View className="flex-row items-center space-x-1">
+                <Feather size={22} name="star" color="#00CCBB"/>
+                <Text className={"text-gray-500"}>{rating} · {genre}
+                </Text>
+              </View>
+              <View className="flex-row items-center space-x-1">
+                <Feather size={22} name="map-pin" color="#00CCBB"/>
+                <Text className={"text-gray-500"}>{address}
+                </Text>
+              </View>
             </View>
-            <View className="flex-row items-center space-x-1">
-              <Feather size={22} name="map-pin" color="#00CCBB"/>
-              <Text className={"text-gray-500"}>{address}
-              </Text>
-            </View>
+            <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
           </View>
-          <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+
+          <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+            <Feather size={22} name="help-circle" color="gray"/>
+            <Text className="pl-2 flex-1 text-md font-bold"> Have a food allergy?</Text>
+            <Feather size={22} name="chevron-right" color="#00CCBB"/>
+          </TouchableOpacity>
         </View>
 
-        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
-          <Feather size={22} name="help-circle" color="gray"/>
-          <Text className="pl-2 flex-1 text-md font-bold"> Have a food allergy?</Text>
-          <Feather size={22} name="chevron-right" color="#00CCBB"/>
-        </TouchableOpacity>
-      </View>
+        <View className="pb-36">
+          <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+          {/*Dishes*/}
+          {dishes.map((dish, index) => (
+            <DishRow
+              key={dish._id}
+              id={dish._id}
+              name={dish.name}
+              description={dish.short_description}
+              price={dish.price}
+              image={dish.image}
+            />))}
+        </View>
+      </ScrollView>
+    </>
 
-      <View className="">
-        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
-
-        {/*Dishes*/}
-        {dishes.map((dish, index) => (
-          <DishRow
-            key={dish._id}
-            id={dish._id}
-            name={dish.name}
-            description={dish.short_description}
-            price={dish.price}
-            image={dish.image}
-          />))}
-      </View>
-    </ScrollView>
   )
 }
 
