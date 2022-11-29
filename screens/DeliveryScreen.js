14457f0a201ec85ed558b5039b76019bf637d376
LuisJo Ruano <luis.jose@arcadalabs.com>
2022-11-29 11:19:16 -0600

    ADD: delivery screen 1/2

---
+++ screens/DeliveryScreen.js
@@ -0,0 +1,12 @@
+import {View, Text, SafeAreaView} from 'react-native'
+import React from 'react'
+
+const DeliveryScreen = () => {
+    return (
+        <SafeAreaView className="">
+            <Text>DeliveryScreen</Text>
+        </SafeAreaView>
+    )
+}
+
+export default DeliveryScreen
